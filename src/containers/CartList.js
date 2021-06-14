e81fc6a8b3c075df30e606386bb9595b292ace50
Gregory Kozlov <mr.gregkozlov@gmail.com>
2021-06-14 18:14:31 +0300

    launch commit

---
+++ src/containers/CartList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import {
+    selectGoods
+} from '../store/goodsSlice';
+
+import {
+    selectCart
+} from '../store/cartSlice';
+
+function CartList() {
+    const goods = useSelector(selectGoods);
+    const cart = useSelector(selectCart);
+    // переиндексирую массив товара
+    const goodsObj = goods.reduce((accum, item) => {
+        accum[item['articul']] = item;
+        return accum;
+    }, {});
+
+    return (
+        <div>
+            <ul>
+                {Object.keys(cart).map(item => <li key={item + goodsObj[item]['title']}>{goodsObj[item]['title']} - {cart[item]}</li>)}
+            </ul>
+        </div>
+    );
+}
+
+export default CartList;
